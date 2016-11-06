14051b4bbd4202ceef58e2b7875b7768cb9162ee
Alexandr <alexgrischuk@gmail.com>
2016-11-06 21:27:02 +0100

    Add button

--- index.ios.js
+++ index.ios.js
@@ -9,13 +9,20 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity
 } from 'react-native';
+
+const circleImage = require('./assets/circle.png')
 
 export default class RelaxationStation extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={() => { alert('I am pressed') }}>
+          <Image source={circleImage} style={styles.buttonImage} />
+        </TouchableOpacity>
         <Text style={styles.readyText}>I'm ready to relax...</Text>
       </View>
     );
@@ -33,6 +40,20 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontStyle: 'italic',
     color: '#ffffff'
+  },
+  button: {
+    backgroundColor: '#859a9b',
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 20,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+  },
+  buttonImage: {
+    width: 200,
+    height: 200
   }
 });
 
