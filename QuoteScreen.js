da2e61044b9fedc3a982bc211520f17b68a82f1b
Alexandr <alexgrischuk@gmail.com>
2016-11-09 15:52:59 +0100

    Add background image

--- QuoteScreen.js
+++ QuoteScreen.js
@@ -2,15 +2,20 @@ import React, { Component, PropTypes } from 'react'
 import {
   StyleSheet,
   View,
+  Image,
 } from 'react-native'
 import Quote from './Quote'
+
+const bgImage = require('./assets/bg.png')
 
 class QuoteScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Quote quoteText={this.props.text} quoteSource={this.props.source} />
-      </View>
+      <Image source={bgImage} style={styles.backgroundContainer}>
+        <View style={styles.container}>
+          <Quote quoteText={this.props.text} quoteSource={this.props.source} />
+        </View>
+      </Image>
     );
   }
 }
@@ -21,6 +26,12 @@ QuoteScreen.propTypes = {
 }
 
 const styles = StyleSheet.create({
+  backgroundContainer: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: undefined,
+    height: undefined
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
